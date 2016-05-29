e6846f466f619d88bc9406bf69ff5e204941fa30
sogou <815319775@qq.com>
2016-05-29 18:00:32 +0800

    f

---
+++ DJHomeFramework/DJHomeFramework/HomeVC/HomeVC.h
@@ -0,0 +1,14 @@
+//
+//  HomeVC.h
+//  LOFFmpegSample
+//
+//  Created by sogou on 16/5/10.
+//  Copyright © 2016年 lewis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeVC : UIViewController
+
+
+@end
