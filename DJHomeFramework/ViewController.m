1eaca307ae10fbb9615091b03e8f6bbd9f9d1bcc
sogou <815319775@qq.com>
2016-05-30 09:31:08 +0800

    组件化

--- DJHomeFramework/ViewController.m
+++ DJHomeFramework/ViewController.m
@@ -7,9 +7,9 @@
 //
 
 #import "ViewController.h"
-
+#import "HomeVC.h"
 @interface ViewController ()
-
+@property (nonatomic , strong) HomeVC *homeVC;
 @end
 
 @implementation ViewController
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _homeVC = [[HomeVC alloc] init];
+    _homeVC.view.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    
+    [self.view addSubview:_homeVC.view];
 }
 
 - (void)didReceiveMemoryWarning {
